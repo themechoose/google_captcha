a5d676dbcd26340e8836bed411d2e63ea983c84f
ThemeChoose <32772691+themechoose@users.noreply.github.com>
2019-04-05 16:04:22 +0600

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,61 @@
+<?php
+    
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $secret_key = '6LeOUJwUAAAAACo7ut7eWfpEI-NqyCFsXvUGBPeC'; /*Here replace your google captcha secret key*/
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+        $name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $subject = strip_tags(trim($_POST["subject"]));
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
+        
+        if(isset($_POST['g-recaptcha-response'])){
+            $captcha = $_POST['g-recaptcha-response'];
+        }
+        
+        // Check that data was sent to the mailer.
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($captcha)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Please fill all the form inputs and check the captcha to submit.";
+            exit;
+        }
+
+        // Set the recipient email address.
+        // FIXME: Update this to your desired email address.
+        $recipient = "egmashraful@gmail.com";
+
+        // Set the email subject.
+        $subject = "New Message from $name";
+
+        // Build the email content.
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Subject: $subject\n\n";
+        $email_content .= "Message:\n$message\n";
+
+        // Build the email headers.
+        $email_headers = "From: $name <$email>";
+        
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret_key=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+        $decoded_response = json_decode($response, true);
+        
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thanks! Message has been sent successfully.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong. Please try again.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "Oops! Something went wrong.";
+    }
+
+?>
